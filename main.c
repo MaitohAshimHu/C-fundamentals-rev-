22d1783c347a78ae326b8089e03cc3b3053b2039
MaitohAshimHu <ashim.dev.sec@gmail.com>
2026-09-13 00:12:33 +0530

    done

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
-int main(void) {
-  int w, h;
-  scanf("%d", &w);
-  scanf("%d", &h);
-  printf("%d\n", w * h);
-  return 0;
-}
+  int main(void) {
+	  int nums[5];
+	  for (int i=0; i<5; i++){
+		  scanf("%d\n" , &nums[i]);
+	  }
+
+	  int best = nums[0];
+
+	  for (int i=1; i<5; i++){
+		  if(best < nums[i])
+			  best = nums[i];
+	  }
+
+	  printf("%d\n" , best);
+	  return 0;
+  }
