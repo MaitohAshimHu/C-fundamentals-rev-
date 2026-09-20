682c3561361e55630fb3d8ebbe151fad484e6fa0
MaitohAshimHu <ashim.dev.sec@gmail.com>
2026-09-20 15:44:37 +0530

    done

--- main.c
+++ main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
-#include <string.h>
 
-  int main(void) {
-	  char buf[100];
-	  fgets(buf, sizeof buf, stdin);
+int main(void) {
+	int n;
+	scanf("%d", &n);
 
-	  size_t len = strlen(buf);
-	  if (len > 0 && buf[len - 1] == '\n'){
-		  buf[len - 1] = '\0';
+   if(n % 15 ==0){
+	   printf("FizzBuzz\n");
+
 	  }
 
-	  printf("%zu\n", strlen(buf));
+   else if(n % 3 ==0){
+	   printf("Fizz\n");
+}
+   else if(n % 5==0){
+	   printf("Buzz\n");
+   }
+else{ printf("%d\n" , n);}
+
+
+
 	  return 0;
   }
