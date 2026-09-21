8cd95f971b560f1647f84695864bd5f3f7ad3641
MaitohAshimHu <ashim.dev.sec@gmail.com>
2026-09-21 17:38:02 +0530

    done

---
+++ fileio.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+	int n;
+	scanf("%d", &n);
+
+	FILE *out = fopen("/tmp/clines.txt", "w");
+
+	if (out == NULL)
+		return 1;
+
+	char buf[1024];
+
+	for(int i = 0 ; i < n; i++) {
+
+		if(fgets(buf, sizeof(buf), stdin) !=NULL) {
+
+			fputs(buf , out);
+
+			size_t len = strlen(buf);
+		
+		if (len >0 && buf[len-1] != '\n')
+		
+		fputc('\n', out);
+	
+		}
+
+	}
+fclose(out);
+
+
+FILE *in = fopen("/tmp/clines.txt", "r");
+
+if (in == NULL)
+	return 1;
+
+int count = 0;
+
+while (fgets(buf, sizeof(buf), in) != NULL) {
+	count++;
+}
+
+	fclose(in);
+
+printf("lines: %d\n", count);
+
+return 0;
+
+}
+    
